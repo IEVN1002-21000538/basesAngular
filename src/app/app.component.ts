ac216bc13d103053d92270ad47bc3398700654b6
Ana Paulina Pedroza Villegas <anapau1pv@gmail.com>
2024-09-18 21:02:24 -0600

    primer commit miercoles 18

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+//decorador qu enos indica tres propiedades principalesque forman un componente
 @Component({
   selector: 'app-root',
+  /*template:`
+  <div class='container'>
+    <div class='row'>
+      <div class='col-md-12'>
+        <h1>Angular 2+ Tutorial</h1>
+      </div>
+      </div>
+      </div>
+  `,*/
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+
 export class AppComponent {
   title = 'basesAngular';
+  dobleNumero(x:number):number{
+    return x*2;
+  }
+  alumno={
+    nombre:'Juan',
+    edad:25,
+    curso:'Angular',
+    fechaInscrito:new Date(),
+    pago:2220
+  }
 }

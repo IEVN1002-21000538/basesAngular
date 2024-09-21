f64bf2f2e3663d2a6f36ff172651e52a44c169c4
Ana Paulina Pedroza Villegas <anapau1pv@gmail.com>
2024-09-21 10:18:13 -0600

    sabado 21 ejemplo1

---
+++ src/app/product/productos.ts
@@ -0,0 +1,12 @@
+export interface IProductos {
+    productoId:number;
+      Modelo:string;
+      Descripcion:string;
+      Precio:number;
+      Year:string;
+      Marca:string;
+      Color:string;
+      // se tiene que copiar la direccion de imagen con el click derecho
+      ImagenUrl:string
+}
+

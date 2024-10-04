43edfef7098a18b53beb1fc6e949c292d4ff5f6c
Ana Paulina Pedroza Villegas <anapau1pv@gmail.com>
2024-10-04 17:40:39 -0600

    Resistencia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ProductoFilterPipe } from './product/producto-filter.pipe';
 import { Ejemplo1Component } from './formularios/ejemplo1/ejemplo1.component';
 import { CinepolisComponent } from './formularios/cinepolis/cinepolis.component';
+import { ResistenciaComponent } from './formularios/resistencia/resistencia.component';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { CinepolisComponent } from './formularios/cinepolis/cinepolis.component'
     ProductListComponent,
     ProductoFilterPipe,
     Ejemplo1Component,
-    CinepolisComponent
+    CinepolisComponent,
+    ResistenciaComponent
   ],
   imports: [
     BrowserModule,
